042de41eb6d32c239153512f59edfb1c0ef16fb9
LuiFeAn <luisfernandogvv@gmail.com>
2024-01-18 02:30:31 -0300

    feat: Criando layout para haver a pre-renderização no servidor antes de o client poder acessar a rota dee redefinição de senha

--- src/app/redefine-password/[jwt]/page.tsx
+++ src/app/redefine-password/[jwt]/page.tsx
@@ -5,14 +5,20 @@ import InputLabel from "@/components/input-label"
 import DefaultButton from "@/components/lib/buttons/default-button"
 import DefaultInput from "@/components/lib/inputs/default-input"
 
-export default function Redefine(props: {
-    params:{
-        jwt: string
+export interface IRedefineProps {
+
+    payload:{
+        payload:{
+            jwt: string
+        }
     }
-}){
+
+}
+
+export default function Redefine({ payload }: IRedefineProps){
 
     const { redefinePasswordForm, submitForm } = useRedefinePassword({
-        jwt: props.params.jwt
+        jwt: payload.payload.jwt
     });
 
     return (
@@ -20,7 +26,7 @@ export default function Redefine(props: {
 
             <InputLabel error={redefinePasswordForm.getter.errors.password}>
             
-                <DefaultInput { ...redefinePasswordForm.getter.getFieldProps("password")} placeholder="Nova senha"/>
+                <DefaultInput { ...redefinePasswordForm.getter.getFieldProps("password")} type="password" placeholder="Nova senha"/>
 
             </InputLabel>
 
