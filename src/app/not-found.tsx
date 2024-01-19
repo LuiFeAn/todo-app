34ed7870e0005a30982dfa0268ad9433b6a18ed0
LuiFeAn <luisfernandogvv@gmail.com>
2024-01-18 22:33:53 -0300

    feat: Adicionando página customizada 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+
+export default function NotFoundPage(){
+
+    return (
+        <div className="text-center">
+
+            <h1  className="font-bold size text-6xl h-screen">Ops ! página não existente </h1>
+
+        </div>
+    )
+
+}
